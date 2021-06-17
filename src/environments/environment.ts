edcbe25af61cf192c9aaded4d31994e2cfab9d4e
Saiful Khan <saiful.etc@gmail.com>
2021-06-17 21:19:01 +0100

    commit before updating angular 11 to 12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -25,3 +25,27 @@ export const environment: any = {
         measurementId: 'xx',
     },
 };
+
+/*
+export const environment = {
+    production: true,
+    staging: false,
+    configuration: 'production',
+    components: {
+        'API_JS': 'http://vis.scrc.uk/api/v1', // Node
+        'API_PY': 'http://vis.scrc.uk/stat/v1', // Python
+        'sentry': '',
+        'VIS_URL': 'http://vis.scrc.uk',
+    },
+    firebaseConfig: {
+        'apiKey': 'xx',
+        'authDomain': 'xx',
+        'databaseURL': 'xx',
+        'projectId': 'xx',
+        'storageBucket': 'xx',
+        'messagingSenderId': 'xx',
+        'appId': 'xx',
+        'measurementId': 'xx',
+    },
+};
+*/
