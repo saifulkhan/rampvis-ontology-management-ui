a7367da35421a708c7964660f53e1081460e59c3
Saiful Khan <saiful.etc@gmail.com>
2021-12-22 12:36:13 +0000

    add external API in list of url code

--- src/app/models/ontology/onto-data-types.ts
+++ src/app/models/ontology/onto-data-types.ts
@@ -1,32 +1,32 @@
 export enum DATA_TYPE {
-    TIMESERIES = 'timeseries',
-    MULTI_TIMESERIES = 'multi_timeseries',
-    CUM_TIMESERIES = 'cum_timeseries',
-    MULTI_CUM_TIMESERIES = 'multi_cum_timeseries',
-    MATRIX = 'matrix',
-    OTHER = 'other',
+  TIMESERIES = "timeseries",
+  MULTI_TIMESERIES = "multi_timeseries",
+  CUM_TIMESERIES = "cum_timeseries",
+  MULTI_CUM_TIMESERIES = "multi_cum_timeseries",
+  MATRIX = "matrix",
+  OTHER = "other",
 }
 
 export enum URL_CODE {
-    API_JS = 'API_JS',
-    API_PY = 'API_PY',
+  API_JS = "API_JS",
+  API_PY = "API_PY",
+  API_EXTERNAL = "API_EXTERNAL",
 }
 
 export enum SOURCE {
-    SCOTLAND = 'scotland',
-    HOSPITAL = 'hospital',
+  SCOTLAND = "scotland",
+  HOSPITAL = "hospital",
 }
 
 export enum MODEL {
-    IC = 'ic',
-    ONEKM2 = '1km2',
-    LHSTM = 'lhstm',
-    CT = 'c&t',
-    EERA = 'eera',
+  IC = "ic",
+  ONEKM2 = "1km2",
+  LHSTM = "lhstm",
+  CT = "c&t",
+  EERA = "eera",
 }
 
 export enum ANALYTICS {
-    SIMILARITY = 'similarity',
-    UNCERTAINTY = 'uncertainty',
+  SIMILARITY = "similarity",
+  UNCERTAINTY = "uncertainty",
 }
-
