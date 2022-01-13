d01b20e539bcd6ce0dd36538650e85af0f20bcc4
Saiful Khan <saiful.etc@gmail.com>
2022-01-13 18:30:46 +0000

    show external data

--- src/app/components/onto-data/show/show.component.ts
+++ src/app/components/onto-data/show/show.component.ts
@@ -1,57 +1,62 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { ReplaySubject } from 'rxjs';
+import { Component, Inject, OnInit, ViewChild } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { ReplaySubject } from "rxjs";
 
-import { APIService } from '../../../services/api.service';
-import { OntoData } from '../../../models/ontology/onto-data.model';
-import { environment } from '../../../../environments/environment';
+import { APIService } from "../../../services/api.service";
+import { OntoData } from "../../../models/ontology/onto-data.model";
+import { environment } from "../../../../environments/environment";
+import { URL_CODE } from "../../../models/ontology/onto-data-types";
 
 @Component({
-    selector: 'app-onto-data-show',
-    templateUrl: './show.component.html',
-    styleUrls: ['./show.component.scss'],
+  selector: "app-onto-data-show",
+  templateUrl: "./show.component.html",
+  styleUrls: ["./show.component.scss"],
 })
 export class OntoDataShowComponent implements OnInit {
-    @ViewChild('modalForm') modalForm: any;
+  @ViewChild("modalForm") modalForm: any;
 
-    public length$: ReplaySubject<number> = new ReplaySubject<number>(1);
-    public jsonData$: ReplaySubject<[]> = new ReplaySubject<[]>(1);
-    public column$: ReplaySubject<[]> = new ReplaySubject<[]>(1);
+  public length$: ReplaySubject<number> = new ReplaySubject<number>(1);
+  public jsonData$: ReplaySubject<[]> = new ReplaySubject<[]>(1);
+  public column$: ReplaySubject<[]> = new ReplaySubject<[]>(1);
 
-    public data!: OntoData;
-    public url = '';
-    spinner = true;
+  public data!: OntoData;
+  public url = "";
+  spinner = true;
 
-    constructor(
-        public matDialogRef: MatDialogRef<OntoDataShowComponent>,
-        @Inject(MAT_DIALOG_DATA) data: any,
-        private api: APIService
-    ) {
-        console.log('OntoDataShowComponent: data = ', data);
-        this.data = data;
+  constructor(
+    public matDialogRef: MatDialogRef<OntoDataShowComponent>,
+    @Inject(MAT_DIALOG_DATA) data: any,
+    private api: APIService
+  ) {
+    console.log("OntoDataShowComponent: data = ", data);
+    this.data = data;
 
-        const urlCode: string = this.data.urlCode;
-        this.url = `${(environment.components as any)[urlCode]}${this.data.endpoint}`;
-
-        this.api.get(this.url).subscribe((res: any) => {
-            console.log('OntoDataShowComponent: res = ', res);
-
-            if (res && Array.isArray(res) && res.length > 0) {
-                this.jsonData$.next((res as any).slice(Math.max(res.length - 10, 0)));
-                this.length$.next(res.length);
-                this.column$.next(Object.keys(res[0]) as any);
-                this.spinner = false;
-            } else if (res) {
-                this.jsonData$.next(res);
-                this.length$.next(0);
-                this.spinner = false;
-            }
-        });
+    const urlCode: URL_CODE = this.data.urlCode;
+    if (urlCode === URL_CODE.API_JS || urlCode === URL_CODE.API_PY) {
+      this.url = `${(environment.components as any)[urlCode]}${this.data.endpoint}`;
+    } else {
+      this.url = this.data.endpoint;
     }
 
-    ngOnInit(): void {}
+    this.api.get(this.url).subscribe((res: any) => {
+      console.log("OntoDataShowComponent: res = ", res);
 
-    public close() {
-        this.matDialogRef.close();
-    }
+      if (res && Array.isArray(res) && res.length > 0) {
+        this.jsonData$.next((res as any).slice(Math.max(res.length - 10, 0)));
+        this.length$.next(res.length);
+        this.column$.next(Object.keys(res[0]) as any);
+        this.spinner = false;
+      } else if (res) {
+        this.jsonData$.next(res);
+        this.length$.next(0);
+        this.spinner = false;
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+
+  public close() {
+    this.matDialogRef.close();
+  }
 }
--- src/app/models/ontology/onto-data.model.ts
+++ src/app/models/ontology/onto-data.model.ts
@@ -1,32 +1,32 @@
-import { Deserializable } from 'src/app/models/deserializable.model';
-import { DATA_TYPE } from '../../models/ontology/onto-data-types';
+import { Deserializable } from "src/app/models/deserializable.model";
+import { DATA_TYPE, URL_CODE } from "../../models/ontology/onto-data-types";
 
 export class OntoData implements Deserializable {
-    public id!: string;
-    public urlCode!: string;
-    public endpoint!: string;
-    public dataType!: DATA_TYPE;
-    public description!: string;
-    public date!: Date;
-    public keywords!: string[];
+  public id!: string;
+  public urlCode!: URL_CODE;
+  public endpoint!: string;
+  public dataType!: DATA_TYPE;
+  public description!: string;
+  public date!: Date;
+  public keywords!: string[];
 
-    deserialize(input: any) {
-        Object.assign(this, input);
-        return this;
-    }
+  deserialize(input: any) {
+    Object.assign(this, input);
+    return this;
+  }
 }
 
 export class OntoDataSearch extends OntoData {
-    public score: number = undefined as any;
-    public pageIds: string[] = undefined as any;
+  public score: number = undefined as any;
+  public pageIds: string[] = undefined as any;
 }
 
 export class OntoDataSearchGroup implements Deserializable {
-    public score: number = undefined as any;
-    public groups: OntoData[] = undefined as any;
+  public score: number = undefined as any;
+  public groups: OntoData[] = undefined as any;
 
-    deserialize(input: any) {
-        Object.assign(this, input);
-        return this;
-    }
+  deserialize(input: any) {
+    Object.assign(this, input);
+    return this;
+  }
 }
